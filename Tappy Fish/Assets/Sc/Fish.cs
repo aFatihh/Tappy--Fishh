b063a4f3ffafa99bfdaa34d2da4e938155a21adb
A.Fatih <rep_ahmet46@hotmail.com>
2023-06-01 14:55:40 +0300

    add whole game
    add whole game

---
+++ Tappy Fish/Assets/Sc/Fish.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float speed;
+    int angle;
+    int maxAngel = 20;
+    int minAngel = -60;
+    public Score score;
+    public GameManager gameManager;
+    public Sprite fishDead;
+    SpriteRenderer sp;
+    Animator anim;
+
+    bool touchedGround;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+   
+    void Update()
+    {
+        FishSwim();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        FishRotation();
+    }
+
+    void FishSwim()
+    {
+        if (Input.GetMouseButtonDown(0) && GameManager.gameOver == false)
+        {
+            _rb.velocity = Vector2.zero;
+            _rb.velocity = new Vector2(_rb.velocity.x, speed);
+        }
+    }
+
+    void FishRotation()
+    {
+        if (_rb.velocity.y > 0)
+        {
+            if (angle <= maxAngel)
+            {
+                angle = angle + 4;
+            }
+
+        }
+        else if (_rb.velocity.y < -2.5f)
+        {
+            if (angle > minAngel)
+            {
+                angle = angle - 2;
+            }
+        }
+
+        if (touchedGround == false)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            score.Scored();
+        }
+        else if (collision.CompareTag("Column"))
+        {
+            // game over
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (GameManager.gameOver == false)
+            {
+                // game over
+                gameManager.GameOver();
+                GameOver();
+            }
+            
+        }
+            
+    }
+
+    void GameOver()
+    {
+        touchedGround = true;
+        sp.sprite = fishDead;
+        anim.enabled = false;
+        transform.rotation = Quaternion.Euler(0, 0, -99);
+    }
+
+}
